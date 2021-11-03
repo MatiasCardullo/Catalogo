e826e372b71f2a7c8220364ef73f480035238f36
matyz97 <supermaty97@gmail.com>
2021-11-03 11:18:51 -0300

    deleted pastebin script

--- Catalogo/Catalogo.cs
+++ Catalogo/Catalogo.cs
@@ -4,7 +4,6 @@ using System.IO;
 using System.Collections.Generic;
 using iTextSharp.text.pdf;
 using iTextSharp.text;
-using System.Net;
 
 namespace Catalogo
 {
@@ -100,67 +99,6 @@ namespace Catalogo
         }
         private void button4_Click(object sender, EventArgs e)
         {
-            
-            /* fuck pastebin
-            string text = Clipboard.GetText();
-            if (text.Length != 0 && textBox1.Text.Length != 0)
-            {
-            Upload:
-
-                System.Collections.Specialized.NameValueCollection Data
-                    = new System.Collections.Specialized.NameValueCollection();
-
-                Data["api_paste_name"] = "Catalogo.txt"; //Title
-                Data["api_paste_expire_date"] = "2W"; //2 Weeks
-                Data["api_paste_code"] = "here is the content";
-                Data["api_dev_key"] = "";
-                Data["api_option"] = "paste";
-                Data["$api_paste_private"] = "0"; // 0=public 1=unlisted 2=private
-
-                WebClient wb = new WebClient();
-                byte[] bytes = wb.UploadValues("http://pastebin.com/api/api_post.php", Data);
-
-                string response;
-                using (MemoryStream ms = new MemoryStream(bytes))
-                using (StreamReader reader = new StreamReader(ms))
-                    response = reader.ReadToEnd();
-
-                if (response.StartsWith("Bad API request"))
-                {
-                    if (MessageBox.Show("Failed to upload!", "Error", MessageBoxButtons.RetryCancel, MessageBoxIcon.Error) == DialogResult.Retry)
-                        goto Upload;
-                }
-                else
-                {
-                    response = "http://pastebin.com/raw.php?i=" + response.Substring(20);
-
-                    Clipboard.SetText(response);
-                    MessageBox.Show("YOUR PASTE: " + response);
-                    //MessageBox.Show("Succesfully uploaded! \r\nLink copied to clipboard.", "Success!");
-                    System.Diagnostics.Process.Start(response);
-                }
-
-            }
-            else
-            {
-                MessageBox.Show("NO DATA IN CLIPBOARD!");
-            }*/
-
-            /*string apiKey = "";
-            var client = new PasteBinClient(apiKey);
-            var entry = new PasteBinEntry
-            {
-                Title = "PasteBinCatalogo",
-                Text = "Here it goes the content",
-                Expiration = PasteBinExpiration.OneMonth,
-                Private = false,
-                Format = "None"
-            };
-
-            string pasteUrl = client.Paste(entry);
-            MessageBox.Show("Your paste is published at this URL: " + pasteUrl);*/
-
-
             if (dataGridView1.Rows.Count > 0)
             {
                 bool ErrorMessage = false;
